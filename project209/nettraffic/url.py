8b80604aaa1ded2607d8fd51067b7b5774c6d4ee
Krishna\Krishna.R.K <krishna.rattihallikantharaj@sjsu.edu>
2017-05-08 23:17:04 -0700

    Adding page for displaying blacklisted IP's

--- project209/nettraffic/url.py
+++ project209/nettraffic/url.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     url(r'^$',views.index, name='index'),
     url(r'^findAllIPs/',views.findAllIPs),
+    url(r'^findAllIPs1/',views.findAllIPs1),
     url(r'^findBLAccessingIPs/',views.findBLAccessingIPs),
     url(r'^findDownloads/',views.findDownloads),
 ]
